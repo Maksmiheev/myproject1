812123f17ce49fcf6abce8c2e4aa167558e4b4e5
Maksim_ka <miheevm332@gmail.com>
2025-10-20 21:57:10 +0300

    get_data

--- src/masks.py
+++ src/masks.py
@@ -11,10 +11,4 @@ def get_mask_card_number(card_number: str) -> str:
     return result
 
 
-def get_mask_account(account_number: str) -> str:
-    """Возвращает замаскированный банковский счет в формате **XXXX."""
 
-    if len(account_number) < 4 or not account_number.isdigit():
-        raise ValueError("Номер счета должен содержать минимум 4 цифры.")
-
-    return f"**{account_number[-4:]}"
