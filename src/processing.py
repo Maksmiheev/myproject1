062c4fdfac130dab6ddd77a9c94fa7c77d644356
Maksim_ka <miheevm332@gmail.com>
2025-10-27 23:08:06 +0300

    annotation

--- src/processing.py
+++ src/processing.py
@@ -1,8 +1,10 @@
-def filter_by_state(data, state='EXECUTED'):
-    """принимает список словарей и опционально значение для ключа state и возвращает новый список словарей, содержащий только те словари, у которых ключ
-state соответствует указанному значению."""
+from typing import List, Dict, Any
+
+def filter_by_state(data: List[Dict[str, Any]], state: str = 'EXECUTED') -> List[Dict[str, Any]]:
+    """принимает список словарей и опционально значение для ключа state и возвращает новый список словарей,
+    содержащий только те словари, у которых ключ state соответствует указанному значению."""
     return [item for item in data if item.get('state') == state]
 
-def sort_by_date(data, descending=True):
-    """принимает список словарей и необязательный параметр, задающий порядок сортировки и возвращает новый список, отсортированный по дате """
+def sort_by_date(data: List[Dict[str, Any]], descending: bool = True) -> List[Dict[str, Any]]:
+    """принимает список словарей и необязательный параметр, задающий порядок сортировки и возвращает новый список, отсортированный по дате"""
     return sorted(data, key=lambda x: x['date'], reverse=descending)
